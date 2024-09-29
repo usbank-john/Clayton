f3e0c07152db025e8f5ef6a4f367005143420e7c
usbank-john <lindamorgan502@gmail.com>
2024-09-28 19:26:16 -0700

    Add files via upload

---
+++ validate.js
@@ -0,0 +1,9 @@
+function check(form) {
+  if (form.userid.value == "Johnclayton19" && form.pwd.value == "@Jay19200") {
+    return true;
+  } else {
+    alert("Incorrect Password or Username")
+    return false;
+  }
+}
+
